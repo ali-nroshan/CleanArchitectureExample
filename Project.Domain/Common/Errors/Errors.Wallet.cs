ad3a464a1f44e6e001d0a083f388d9c2b29cecb8
Ali <ali.nr.job@gmail.com>
2024-05-05 12:05:54 -0700

    add entities and errors in domain layer

---
+++ Project.Domain/Common/Errors/Errors.Wallet.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+
+namespace Project.Domain.Common.Errors;
+
+public static class Errors
+{
+    public static class Wallet
+    {
+        public static Error DuplicateWallet => Error.Conflict("Wallet.Duplicate");
+        public static Error NotFoundWallet => Error.NotFound("Wallet.NotFound");
+    }
+}
---
+++ Project.Domain/Entities/Wallet.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Domain.Entities;
+
+public class Wallet(Guid walletId, uint walletBalance, DateTime walletCreateDate)
+{
+    public Guid WalletId { get; set; } = walletId;
+    public uint WalletBalance { get; set; } = walletBalance;
+    public DateTime WalletCreateDate { get; set; } = walletCreateDate;
+}
