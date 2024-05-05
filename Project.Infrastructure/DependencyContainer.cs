4a799f83c4cf5613a0fb93e7d5e5b2d841e801e5
Ali <ali.nr.job@gmail.com>
2024-05-05 15:38:02 -0700

    add connection string and wireup dependencies

---
+++ Project.Infrastructure/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.Application.Contracts.Persistence;
+using Project.Infrastructure.Persistence.Repositories;
+using System.Data.SQLite;
+
+namespace Project.Infrastructure;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection RegisterInfrastructureDependency(this IServiceCollection services, IConfiguration configuration)
+    {
+        _ = services.AddSingleton(new SQLiteConnection()
+        {
+            ConnectionString = configuration.GetConnectionString("ProjectDbConnectionString")
+        });
+
+        _ = services.AddScoped<IWalletRepository, WalletRepository>();
+
+        return services;
+    }
+}
