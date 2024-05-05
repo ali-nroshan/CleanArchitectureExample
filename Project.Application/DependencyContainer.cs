0794aa7e4d4b6583e6f71dd83560a70d8a8bc896
Ali <ali.nr.job@gmail.com>
2024-05-05 13:41:19 -0700

    wireup dependencies in application layer

---
+++ Project.Application/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Project.Application;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection RegisterApplicationDependency(this  IServiceCollection services)
+    {
+        services.AddMediatR(mediatR =>
+        {
+            mediatR.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+        return services;
+    }
+}
