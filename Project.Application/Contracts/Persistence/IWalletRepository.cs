285088c219eb36b554b1fb0fac976aa444ab2baf
Ali <ali.nr.job@gmail.com>
2024-05-05 12:10:53 -0700

    repository interface in application layer

---
+++ Project.Application/Contracts/Persistence/IWalletRepository.cs
@@ -0,0 +1,10 @@
+﻿using Project.Domain.Entities;
+
+namespace Project.Application.Contracts.Persistence;
+
+public interface IWalletRepository
+{
+    Task<IEnumerable<Wallet>> GetWalletsAsync();
+    Task<Wallet> GetWalletAsync(Guid walletId);
+    Task AddWallet(Wallet wallet);
+}
